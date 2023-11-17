cef4a5d9c6b242bb07dc1bf0d3bb9ee5a6ef0508
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-17 11:51:09 +0000

    converison of data type

---
+++ .devcontainer/basice/conversion operation.js
@@ -0,0 +1,24 @@
+let score ="33abcd"
+ 
+console.log(typeof score);
+console.log(typeof (score));
+let valueInmunber = Number(score)
+console.log(typeof valueInmunber);
+console.log(valueInmunber);
+
+let isLoggedIn = 1
+let booleanvalue =Boolean(isLoggedIn)
+console.log(booleanvalue);
+console.log(typeof booleanvalue);  
+
+/* "33"=>33
+    "33abs"=>NaN
+    true=>1; false=>0
+    " "=>Fasle
+    "Anything"=>True
+*/
+
+let number = 55
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
