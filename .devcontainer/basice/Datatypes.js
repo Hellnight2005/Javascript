6f2a2e592891a2c0c1dabcdbd607784259e85d0a
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-17 11:06:48 +0000

    DataTypes in js

---
+++ .devcontainer/basice/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all code as new version//
+
+console.log( 3 + 3)
+
+console.log("Abhijeet")
+let name = "Abhijeet" // treat as string//
+let age = 18 // treat as int//
+let isLoggedIn = true // bollean type//
+
+/* number => 2to power 53
+    bigint
+    string=>" "
+    bollean => True/false
+    null=> Standard value
+    undefined => 
+    symbol => unique
+
+    object
+*/
+console.log(typeof name );
+console.log(typeof null ); // object //
+console.log(typeof undefined ); // undefined //
+
+
