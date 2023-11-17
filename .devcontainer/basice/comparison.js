25f313a59d4e1b2725c34eb2f31d2a42b202d3f1
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-17 15:13:35 +0000

    comparison of datatype

---
+++ .devcontainer/basice/comparison.js
@@ -0,0 +1,14 @@
+console.log(5<69);
+console.log(2>69);
+console.log(5==6);
+console.log(5!=6);
+
+console.log("2" > 1)
+// it convert string into int then comparison //
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+// it is special operation of null //
+console.log("2" === 1) 
+// it will false beause of different type //
