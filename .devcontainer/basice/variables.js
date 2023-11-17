9b9d4847a16e95174f58f1dc2ae3f952d15fb516
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-17 10:48:11 +0000

    defined variable ,comment  in js

---
+++ .devcontainer/basice/variables.js
@@ -0,0 +1,22 @@
+const accountId = 14458    // const is use for constant variable//
+let accountEmail = "abhijeetshinde2005@gmail.com"  // [let] and [var] used a variables in js//
+var accountPassword = "12345"
+accountCicty = "Pune" // you can used your name as variable also //
+let accountStatue;
+
+//accountId = 568 constant value is not change //
+accountEmail ="hell@gmail.com" 
+accountPassword = "9820530968"
+accountCicty = "mumbai"
+
+console.log(accountId);
+/*
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCicty);  */
+/* prefer not to use var
+beacuse of issue in block scop and function
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCicty,accountStatue]); 
+// Here we print in table formate and we can assinge many things at same time //
