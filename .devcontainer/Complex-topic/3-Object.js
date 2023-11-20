8e9a4c958a1d161890acfdfc87216b86f99626ed
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-20 10:46:24 +0000

    vfb

---
+++ .devcontainer/Complex-topic/3-Object.js
@@ -0,0 +1,14 @@
+// object  literals 
+
+const jsUser = {
+    name: "Abhijeet",
+    age: 18,
+    location: "pune",
+    email: "hellnignt",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+
+console.log(jsUser.name)
+console.log(jsUser["name"])
