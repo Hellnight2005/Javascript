a7926af3a27208a40dc20b6c2d94e9daaaf3bdbb
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-25 06:39:34 +0000

    Immediately Invoked Function Expression

---
+++ .devcontainer/Complex-Topice/4-IIFE.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression [ IIFE ]
+// name IIFE
+(function Start(){
+    console.log(` DB CONNECTED`)
+}) ();
+
+// Unknow IIFE
+((name)=>{
+    console.log(`DB CONNECTED TWO is ${name}`);
+})(" Abhijeet")
+
