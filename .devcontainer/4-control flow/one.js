73e9e75457043efd46de43ca5ef646dc6e6a425e
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-25 07:18:10 +0000

    if ,else if , esle

---
+++ .devcontainer/4-control flow/one.js
@@ -0,0 +1,39 @@
+// if
+
+/*
+<  lessthan
+>  graeterthan
+<= less than or equal
+>= greater than or equal
+== equal to 
+!= not equal
+=== checked DataType 
+&& And 
+|| or
+
+*/
+
+
+const temp = 39
+if (temp < 50){
+    console.log("Temp is less than 50");
+} else{
+    console.log("Temp is greater than 50");
+}
+/*
+// Second method 
+const balanced = 350
+//if (balanced > 500) console.log("test"); 
+if (balanced <=500){
+    console.log("less than 500");
+}else if (balanced <= 400){
+    console.log("less than 400");
+}else if (balanced <= 300){
+    console.log("less than 300");
+}else if (balanced <= 200){
+    console.log("less than 200");
+}else {
+    console.log("efficient balance");
+}
+*/
+
