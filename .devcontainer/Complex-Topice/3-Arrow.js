174b744635eea3b67fe78ef1482f1ac862cdc4de
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-11-25 06:28:02 +0000

    This And Arrow Function

---
+++ .devcontainer/Complex-Topice/3-Arrow.js
@@ -0,0 +1,34 @@
+const User = {
+    Username: "Abhijeet",
+    Price: 99,
+
+    welcomeMessage: function(){
+        console.log(`${this.Username}, Welcome to Website`);
+        console.log(this);
+    }
+
+}
+
+//User.welcomeMessage()
+//User.Username =  "Yash"
+//User.welcomeMessage()
+
+//console.log(this);
+
+// If you print this in Function 
+function On(){
+    console.log(this);
+}
+//On()
+
+
+// This is Arrow Function 
+const Ontwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(Ontwo(5, 8))
+
+
+// This Shortcut Method for Above Example
+const AddtwoNumber = (num1, num2) => (num1 + num2)
+console.log(AddtwoNumber(5, 8));
