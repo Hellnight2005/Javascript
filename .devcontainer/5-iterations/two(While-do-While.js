c65ffb7760f07540789baa0a502146918ef7b6e0
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2023-12-11 15:43:31 +0000

    (While, Do-While ) syntax

---
+++ .devcontainer/5-iterations/two(While-do-While.js
@@ -0,0 +1,26 @@
+// While syntax 
+let i = 0
+
+while (i <= 10) { 
+   // console.log(`Value of index is ${i}`);
+    i=i+2
+}
+
+
+// While in Array
+let myArray =['Yash', 'Abhijeet', 'raj']
+let Arr = 0
+while (Arr < myArray.length) {
+    console.log(`Names  is ${myArray[Arr]} `);
+    Arr =Arr +1
+}
+
+
+
+// Do while Syntax 
+// condition execute then condition check
+let score = 121
+do {
+    //console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
