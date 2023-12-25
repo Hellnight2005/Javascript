66e77d7333946b5de961b5105dbca13ee713d80b
Hellnight2005 <abhijeet2005shinde.com>
2023-12-25 22:26:07 +0530

    protype of javascript

---
+++ 10-Classes_and_oop/protype.js
@@ -0,0 +1,16 @@
+// let Myname = "Hitesh     "
+
+
+// console.log(Myname.truelength);
+
+let myHero = ["thor","superman"]
+let heroPower = {
+    thor: "hammer",
+    superman: "fly",
+
+    getsupermanpower: function(){
+        console.log(`superman power is ${this.superman}`);
+    }
+}
+
+heroPower.hitesh()
