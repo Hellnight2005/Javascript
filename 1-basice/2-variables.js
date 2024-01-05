a7c68c94f8a36a36ff66cdd45200244d566e88d8
Hellnight2005 <149698466+Hellnight2005@users.noreply.github.com>
2024-01-05 23:37:44 +0530

    Variables

--- 1-basice/2-variables.js
+++ 1-basice/2-variables.js
@@ -13,13 +13,13 @@ accountPassword = "9820530968"
 accountCicty = "mumbai"
 
 console.log(accountId);
-/*
+
 console.log(accountEmail);
 console.log(accountPassword);
-console.log(accountCicty);  */
-/* prefer not to use var
-beacuse of issue in block scop and function
-*/
+console.log(accountCicty);  
+//  prefer not to use var
+// beacuse of issue in block scop and function
+
 
 console.table([accountId,accountEmail,accountPassword,accountCicty,accountStatue]); 
 // Here we print in table formate and we can assinge many things at same time //
